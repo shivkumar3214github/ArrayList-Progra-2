e021c80e5e715362e79a4cc665bff4ec034035d1
shivkumar3214github <shivakumarchandapure1@gmail.com>
2022-11-14 21:21:40 +0530

    my first commit

---
+++ Demo2/src/com/practice/github/ArryListDemo.java
@@ -0,0 +1,23 @@
+package com.practice.github;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class ArryListDemo {
+
+	public static void main(String[] args) {
+		ArrayList al = new ArrayList();
+		al.add(10);
+		al.add(20);
+		al.add(30);
+		al.add(40);
+		System.out.println(al);
+		
+		Iterator itr = al.iterator();
+		while(itr.hasNext()){
+		System.out.println(itr.next());	
+		
+		}
+	}
+
+}
